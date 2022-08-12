611330465807acdfb3e47628b4e0f103de230e42
rklocke <rebeccakatelocke@gmail.com>
2022-08-12 12:45:26 +0100

    Added in JIRA information, added this to plots along with total on each stacked bar, added logging

--- TAT_queries.py
+++ TAT_queries.py
@@ -1,7 +1,9 @@
 import datetime as dt
 import dxpy as dx
 import json
+import logging
 import numpy as np
+import os
 import pandas as pd
 import plotly.graph_objects as go
 import requests
@@ -13,6 +15,7 @@ from requests.auth import HTTPBasicAuth
 
 pd.options.mode.chained_assignment = None
 
+# Get tokens etc from credentials file
 with open("credentials.json", "r") as json_file:
     CREDENTIALS = json.load(json_file)
 
@@ -20,12 +23,38 @@ DX_TOKEN = CREDENTIALS.get('DX_TOKEN')
 JIRA_EMAIL = CREDENTIALS.get('JIRA_EMAIL')
 JIRA_TOKEN = CREDENTIALS.get('JIRA_TOKEN')
 STAGING_AREA_PROJ_ID = CREDENTIALS.get('STAGING_AREA_PROJ_ID')
+JIRA_NAME = CREDENTIALS.get('JIRA_NAME')
 
+# Get path the script is run in
+CURRENT_DIR = os.path.abspath(os.getcwd())
+
+# Create and configure logger
+LOG_FORMAT = "%(asctime)s — %(name)s — %(levelname)s — %(lineno)d — %(message)s"
+
+# Set level to debug, format with date and time and re-write file each time
+logging.basicConfig(
+    filename=f'{CURRENT_DIR}/TAT_queries_debug.log',
+    level=logging.DEBUG,
+    format=LOG_FORMAT,
+    filemode='w')
+
+# Set up logger
+logger = logging.getLogger("main log")
+
+# Jira API things
+auth = HTTPBasicAuth(JIRA_EMAIL, JIRA_TOKEN)
+
+headers = {
+   "Accept": "application/json"
+}
+
+# Audit settings and relevant dates (used in plot titles)
 NO_OF_AUDIT_WEEKS = 12
 
 today_date = dt.date.today()
-x_weeks_before = dt.timedelta(weeks = NO_OF_AUDIT_WEEKS)
-begin_date_of_audit = today_date - x_weeks_before
+x_weeks = dt.timedelta(weeks = NO_OF_AUDIT_WEEKS)
+begin_date_of_audit = today_date - x_weeks
+
 
 def login() -> None:
     """
@@ -49,9 +78,9 @@ def login() -> None:
 
     try:
         dx.api.system_whoami()
-        print("DNAnexus login successful")
+        logger.info("DNAnexus login successful")
     except Exception as err:
-        print("Error logging in to DNAnexus")
+        logger.error("Error logging in to DNAnexus")
         sys.exit(1)
 
 
@@ -156,11 +185,9 @@ def add_log_file_time(run_dict):
 
         # For key with relevant run name
         # Add log_file_created key with time logfile created in datetime format
-        log_time = (
-            log_file_info[0]['describe']['created'] / 1000
-        )
+        log_time = log_file_info[0]['describe']['created'] / 1000
         run_dict[run_name]['log_file_created'] = (
-            f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(log_time))}"
+            time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(log_time))
         )
 
     return run_dict
@@ -200,7 +227,7 @@ def find_earliest_002_job(run_dict):
         # For key with relevant run name
         # Add earliest_002_job key with time of earliest job in datetime format
         run_dict[run]['earliest_002_job'] = (
-            f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(first_job))}"
+            time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(first_job))
         )
 
     return run_dict
@@ -251,7 +278,7 @@ def add_successful_multiQC_time(run_dict):
                 multiqc_fin = jobs[0]['describe']['stoppedRunning'] / 1000
 
             multi_qc_completed = (
-                f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(multiqc_fin))}"
+                time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(multiqc_fin))
             )
 
         else:
@@ -286,6 +313,8 @@ def create_info_dict(assay_type):
     )
     assay_run_dict = add_successful_multiQC_time(assay_run_dict)
     find_earliest_002_job(assay_run_dict)
+
+    # SNP runs are uploaded manually so don't have lane.all.log file
     if assay_type == 'SNP':
         pass
     else:
@@ -328,8 +357,50 @@ def add_001_demultiplex_job(all_assays_dict):
         demulti_time = job['describe']['created'] / 1000
         if run_name in all_assays_dict:
             all_assays_dict[run_name]['demultiplex_started'] = (
-                f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(demulti_time))}"
+                time.strftime('%Y-%m-%d %H:%M:%S',
+                time.localtime(demulti_time))
             )
+
+    return all_assays_dict
+
+
+def get_jira_info(queue_id, jira_name):
+    url = (
+        f"https://{jira_name}.atlassian.net/rest/servicedeskapi/servicedesk/"
+        f"4/queue/{queue_id}/issue"
+    )
+
+    response = requests.request(
+       "GET",
+       url,
+       headers=headers,
+       auth=auth
+    )
+
+    queue_response = json.loads(response.text)
+
+    return queue_response
+
+
+def add_jira_info(all_assays_dict, jira_response):
+    for issue in jira_response['values']:
+        run_name = issue['fields']['summary']
+
+        if run_name in all_assays_dict:
+            final_status = issue['fields']['status']['name']
+            status_time_misec = (
+                issue['fields']['customfield_10032'][
+                    'completedCycles'
+                ][0]['stopTime']['epochMillis'] / 1000
+            )
+            res_time = (
+                dt.datetime.fromtimestamp(
+                    status_time_misec
+                ).replace(microsecond=0)
+            )
+            res_time_str = res_time.strftime('%Y-%m-%d %H:%M:%S')
+            all_assays_dict[run_name]['final_jira_status'] = final_status
+            all_assays_dict[run_name]['jira_resolved'] = res_time_str
 
     return all_assays_dict
 
@@ -354,15 +425,17 @@ def create_all_assays_df(all_assays_dict):
 
     # Reorder columns
     all_assays_df = all_assays_df[[
-        'assay_type','run_name', 'log_file_created', 'demultiplex_started',
-        '002_project_created', 'earliest_002_job', 'multiQC_finished'
+        'assay_type', 'run_name', 'log_file_created', 'demultiplex_started',
+        '002_project_created', 'earliest_002_job', 'multiQC_finished',
+        'final_jira_status', 'jira_resolved'
     ]]
 
-    # Convert cols to pandas datetime type
     cols_to_convert = [
-        'log_file_created','demultiplex_started','002_project_created',
-        'earliest_002_job', 'multiQC_finished'
+        'log_file_created', 'demultiplex_started', '002_project_created',
+        'earliest_002_job', 'multiQC_finished', 'jira_resolved'
     ]
+
+    # Convert cols to pandas datetime type
     all_assays_df[cols_to_convert] = all_assays_df[cols_to_convert].apply(
         pd.to_datetime, format='%Y-%m-%d %H:%M:%S'
     )
@@ -384,7 +457,7 @@ def add_calculation_columns(all_assays_df):
         dataframe with a row for each run and extra columns
     """
     # Add new column for type between log file and earliest 002 job
-    all_assays_df['log_file_to_first_002_job_days'] = (
+    all_assays_df['log_file_to_first_002_job'] = (
         (all_assays_df['earliest_002_job'] - all_assays_df['log_file_created'])
         / np.timedelta64(1, 'D')
     )
@@ -393,6 +466,18 @@ def add_calculation_columns(all_assays_df):
     # To the time that the last successful MultiQC job finished
     all_assays_df['processing_time'] = (
         (all_assays_df['multiQC_finished'] - all_assays_df['earliest_002_job'])
+        / np.timedelta64(1, 'D')
+    )
+
+    # Add new column for time from MultiQC end to Jira resolution
+    all_assays_df['processing_end_to_release'] = (
+        (all_assays_df['jira_resolved'] - all_assays_df['multiQC_finished'])
+        / np.timedelta64(1, 'D')
+    )
+
+    # Add new column for time from log file creation to Jira resolution
+    all_assays_df['upload_to_release'] = (
+        (all_assays_df['jira_resolved'] - all_assays_df['log_file_created'])
         / np.timedelta64(1, 'D')
     )
 
@@ -434,9 +519,9 @@ def create_TAT_fig(assay_df, assay_type):
     fig.add_trace(
         go.Bar(
             x=assay_df["run_name"],
-            y=assay_df["log_file_to_first_002_job_days"],
+            y=assay_df["log_file_to_first_002_job"],
             name="Upload to processing start",
-            legendrank=2
+            legendrank=3
         )
     )
 
@@ -446,15 +531,37 @@ def create_TAT_fig(assay_df, assay_type):
             x=assay_df["run_name"],
             y=assay_df["processing_time"],
             name="Pipeline running",
-            legendrank=1
+            legendrank=2
         )
+    )
+
+    fig.add_trace(
+        go.Bar(
+            x=assay_df["run_name"],
+            y=assay_df["processing_end_to_release"],
+            name="End of processing to release",
+            legendrank=1,
+            text=round(assay_df['upload_to_release'])
+        )
+    )
+
+    fig.add_hline(y=3, line_dash="dash")
+
+    fig.update_traces(
+        hovertemplate=(
+            '<br><b>Run</b>: %{x}<br>'
+            '<b>Stage</b>: %{data.name}<br>'
+            '<b>Days</b>: %{y:.2f}<br>'
+            '<extra></extra>'
+        ),
+        textposition='outside'
     )
 
     # Update relevant aspects of chart
     fig.update_layout(
         barmode='stack',
         title={
-            'text': f"{assay_type} Turnaround Times - {begin_date_of_audit} -"
+            'text': f"{assay_type} Turnaround Times {begin_date_of_audit} -"
                     f" {today_date}",
             'xanchor': 'center',
             'x': 0.5,
@@ -469,33 +576,38 @@ def create_TAT_fig(assay_df, assay_type):
 
     return fig
 
-    # fig.show()
 
 def main():
-
+    """Main function to create html report"""
     login()
-    
-    print("Creating dicts for each assay")
+
+    logger.info("Creating dicts for each assay")
     all_assays_dict = {}
     assay_types = ['TWE', 'CEN', 'MYE', 'TSO500', 'SNP']
     for assay_type in assay_types:
         all_assays_dict.update(create_info_dict(assay_type))
-    
-    print("Adding demultiplex job")
+
+    logger.info("Adding demultiplex job")
     add_001_demultiplex_job(all_assays_dict)
-    
-    print("Merging to one df")
+
+    logger.info("Getting JIRA ticket info for closed seq runs")
+    closed_runs_response = get_jira_info(35, JIRA_NAME)
+
+    logger.info("Adding closed tickets info to dict")
+    add_jira_info(all_assays_dict, closed_runs_response)
+
+    logger.info("Merging to one df")
     all_assays_df = create_all_assays_df(all_assays_dict)
-    print("Adding TAT days")
+    logger.info("Adding TAT steps info")
     add_calculation_columns(all_assays_df)
-    
-    print("Extracting each df")
+
+    logger.info("Extracting each df")
     CEN_df = extract_assay_df(all_assays_df, 'CEN')
     MYE_df = extract_assay_df(all_assays_df, 'MYE')
     TSO500_df = extract_assay_df(all_assays_df, 'TSO500')
     TWE_df = extract_assay_df(all_assays_df, 'TWE')
     SNP_df = extract_assay_df(all_assays_df, 'SNP')
-    print("Creating figs")
+    logger.info("Creating figs")
     CEN_fig = create_TAT_fig(CEN_df, 'CEN')
     MYE_fig = create_TAT_fig(MYE_df, 'MYE')
     TSO500_fig = create_TAT_fig(TSO500_df, 'TSO500')

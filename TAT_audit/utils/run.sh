021999e77c62f1c782efe568edad9aba07d3419f
Jethro Rainford <rainford1995@gmail.com>
2023-07-27 18:33:23 +0100

    prettier Slack message

--- TAT_audit/utils/run.sh
+++ TAT_audit/utils/run.sh
@@ -15,7 +15,7 @@ python3 TAT_queries.py -s $start -e $end
 # upload HTML report to Slack, and post associated .csv in thread with the file
 response=$(curl -F file=@$(ls *.html) \
     -F channels=$SLACK_CHANNEL \
-    -F "initial_comment=Latest bioinformatics turn around times for between $start and $end" \
+    -F "initial_comment=:bar_chart: Latest bioinformatics turn around times for between \`$start\` and \`$end\`" \
     -H "Authorization: Bearer $SLACK_TOKEN" https://slack.com/api/files.upload)
 
 thread=$(jq -r '.file.shares.private[][].ts' <<< $response)
